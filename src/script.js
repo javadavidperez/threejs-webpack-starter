b087f0122d75344d2542bcbc45677b30afa0f734
David Perez <javadavidperez@gmail.com>
2022-04-26 23:33:31 -0500

    Terraintest

--- src/script.js
+++ src/script.js
@@ -12,25 +12,73 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-// Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+//Texture
+const textuloader = new THREE.TextureLoader()
+const height = textuloader.load('./hmapl.png')
+
+// Mesh
+const planeterr = new THREE.PlaneBufferGeometry(620, 860, 80, 80)
+const csphere = new THREE.SphereGeometry( 75, 3, 2 );
+const sphere = new THREE.SphereGeometry( 15, 6, 6 );
 
 // Materials
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+const material = new THREE.MeshStandardMaterial({
+    color: 0x88ff33,
+    wireframe: true,
+    displacementMap: height,
+    displacementScale: -180
+})
+const spheremat = new THREE.MeshStandardMaterial({
+    color: 0xbbfeb3,
+    wireframe: false,
+})
 
-// Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
+// Objects
+const mainsphere = new THREE.Mesh(csphere, spheremat)
+const secsphere = new THREE.Mesh(sphere, spheremat)
+
+const planeobj = new THREE.Mesh(planeterr, material)
+
+//Terrain
+scene.add(planeobj)
+planeobj.rotation.x = 1.9;
+planeobj.rotation.y = 0;
+planeobj.position.z = -600;
+planeobj.position.x = 0;
+planeobj.position.y = 0;
+
+//Sphere
+scene.add(mainsphere)
+
+mainsphere.position.z = -1250;
+mainsphere.position.y = 340;
+
+//stars
+function addStar() {
+	const geoStar = new THREE.SphereGeometry(1, 3, 3);
+	const materialStar = new THREE.MeshStandardMaterial({ color: 0xfefeee, })
+	const star = new THREE.Mesh( geoStar, materialStar );
+	const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 800 ));
+	// MatUtils randomly generates a number between -100 and 100
+	star.position.set(x, y + 340, z -1350);
+	scene.add(star)
+}
+Array(200).fill().forEach(addStar)
 
 // Lights
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
+const pointLight = new THREE.PointLight(0xffffff, 1)
 pointLight.position.x = 2
 pointLight.position.y = 3
 pointLight.position.z = 4
 scene.add(pointLight)
+const sunLight = new THREE.AmbientLight(0xffff, .6);
+scene.add(sunLight);
+
+
+//UI controls
+gui.add(planeobj.rotation, 'x').min(0).max(2);
 
 /**
  * Sizes
@@ -59,11 +107,16 @@ window.addEventListener('resize', () =>
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 0
+const camera = new THREE.PerspectiveCamera(50, sizes.width / sizes.height, 20, 1000)
+// camera.position.x = 0
 camera.position.y = 0
-camera.position.z = 2
+camera.rotation.x = 0
+camera.position.z = 34
 scene.add(camera)
+
+gui.add(camera.position, 'y').min(-300).max(250);
+gui.add(camera.position, 'x').min(0).max(200);
+gui.add(camera.position, 'z').min(-1000).max(1000);
 
 // Controls
 // const controls = new OrbitControls(camera, canvas)
@@ -86,11 +139,18 @@ const clock = new THREE.Clock()
 
 const tick = () =>
 {
-
     const elapsedTime = clock.getElapsedTime()
 
+    //camera movement
+    // camera.position.x = 0
+    // camera.position.y = 116 * - (.02 * elapsedTime)
+    // camera.position.z = 310 - (2 * elapsedTime)
+    // camera.rotation.x =  10 + (2000 * elapsedTime)
+    mainsphere.rotation.y = .5 * elapsedTime
+
+
     // Update objects
-    sphere.rotation.y = .5 * elapsedTime
+    // planeobj.rotation.z = .5 * elapsedTime
 
     // Update Orbital Controls
     // controls.update()
@@ -103,3 +163,28 @@ const tick = () =>
 }
 
 tick()
+
+function moveCamera () {
+	const t = document.body.getBoundingClientRect().top;
+    if (t > -2500){
+    camera.position.z = 34 +( t * 0.1);
+    // camera.position.y = t * 0.02;
+    camera.rotation.x = 0;
+    }
+    if ( t < -2500 && camera.position.z >= -650 && camera.position.y <=200) {
+        camera.position.z = 34 + ( t * 0.1);
+        // camera.position.y = t * 0.02;
+        camera.position.y = -175 - ( t * 0.05);
+        camera.rotation.x =  0;
+    }
+    if (camera.position.z <= -400 ){
+        camera.rotation.x -= 0.34 + t * 0.00008;
+    }
+	// camera.position.y = t * 0.02;
+	// console.log(camera.position.z + ' z');
+    // console.log(camera.rotation.x + 'rx');
+    // console.log(t + 'jeje');
+
+
+}
+document.body.onscroll = moveCamera;
